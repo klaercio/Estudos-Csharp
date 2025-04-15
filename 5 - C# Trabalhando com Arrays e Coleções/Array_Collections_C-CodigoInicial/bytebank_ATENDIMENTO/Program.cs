f1787908570591a3f170006454932eee3e0cdb5d
klaercio <klaerciosilva@gmail.com>
2025-04-15 09:48:01 -0300

    😁 Feat: Curso5 - Add Class Lista de contas

--- 5 - C# Trabalhando com Arrays e Coleções/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
+++ 5 - C# Trabalhando com Arrays e Coleções/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
@@ -1,4 +1,5 @@
-﻿using bytebank.Modelos.Conta;
+﻿/*using bytebank.Modelos.Conta;
+using bytebank_Atendimento.bytebank.Util;
 
 ContaCorrente[] contasCorrentes = new ContaCorrente[]
 {
@@ -6,3 +7,55 @@ ContaCorrente[] contasCorrentes = new ContaCorrente[]
     new ContaCorrente(874),
     new ContaCorrente(874)
 };
+
+ListaDeContasCorrente lista = new(3);
+
+contasCorrentes[1].Saldo = 1000;
+
+Console.WriteLine(lista[3]);
+
+lista.AdicionarConta(contasCorrentes[0]);
+lista.AdicionarConta(contasCorrentes[1]);
+lista.AdicionarConta(contasCorrentes[2]);
+lista.AdicionarConta(contasCorrentes[2]);
+lista.AdicionarConta(contasCorrentes[2]);
+lista.AdicionarConta(contasCorrentes[2]);
+lista.AdicionarConta(contasCorrentes[2]);
+
+Console.WriteLine(lista.RetornaContaComMaiorSaldo());*/
+
+int[] numeros = new int[5];
+int[] pares  = new int[5];
+int soma = 0, maior = 0, menor = 0, indice  = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Digite o {i}° número:");
+    numeros[i] = int.Parse(Console.ReadLine()!);
+    soma += numeros[i];
+    if(numeros[i] % 2 == 0)
+    {
+        pares[indice] = numeros[i];
+        indice++;
+    }
+}
+
+for (int i = 0;i < numeros.Length;i++)
+{
+    if(numeros[i] > maior)
+    {
+        maior = numeros[i];
+    }else  if(numeros[i] < menor)
+    {
+        menor = numeros[i];
+    }
+}
+
+Console.WriteLine($"Soma: {soma}");
+Console.WriteLine($"Maior:  {maior}");
+Console.WriteLine($"Menor: {menor}");
+Console.Write("Pares:  ");
+foreach(int i in pares)
+{
+    Console.Write($"{i}");
+}
--- 5 - C# Trabalhando com Arrays e Coleções/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
+++ 5 - C# Trabalhando com Arrays e Coleções/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
@@ -2,7 +2,7 @@ using bytebank.Modelos.Conta;
 
 namespace bytebank_Atendimento.bytebank.Util;
 
-internal class ListaDeContasCorrente 
+internal class ListaDeContasCorrente
 {
     private ContaCorrente[] _itens = null;
     private int _proximaPosicao = 0;
@@ -12,7 +12,73 @@ internal class ListaDeContasCorrente
         _itens = new ContaCorrente[tam];
     }
 
-    public void AdicionarConta(ContaCorrente conta) {
+    public void AdicionarConta(ContaCorrente conta)
+    {
+        Console.WriteLine($"Adicionando conta na posição {_proximaPosicao}");
+        VerificarCapacidade(_proximaPosicao + 1);
+        _itens[_proximaPosicao] = conta;
+        _proximaPosicao++;
+
+      
+    }
+
+
+    private void VerificarCapacidade(int tamanhoNecessario)
+    {
+       if(_itens.Length >= tamanhoNecessario)
+        {
+            return;
+        }
+
+        Console.WriteLine("Aumentando a capacidade da lista");
+
+        ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+        for(var i = 0; i  < _itens.Length; i++)
+        {
+            novoArray[i] = _itens[i];
+        }
+
+        _itens = novoArray;
+    }
+
+    public void RemoverConta(ContaCorrente conta)
+    {
+        for(int i =  0; i < _itens.Length; i++)
+        {
+            if (_itens[i] == conta)
+            {
+
+            }
+        }
+    }
+
+    public ContaCorrente RetornaContaComMaiorSaldo()
+    {
+        ContaCorrente ContaComMaiorSaldo = null!;
+        double saldo = 0;
         
+        foreach(ContaCorrente i in _itens)
+        {
+            if(i.Saldo >= saldo)
+            {
+                saldo = i.Saldo;   
+                ContaComMaiorSaldo = i;
+            }
+        }
+
+        return ContaComMaiorSaldo!;
+    }
+
+    public void ExibeLista()
+    {
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            if (_itens[i] != null)
+            {
+                var conta = _itens[i];
+                Console.WriteLine($" Indice[{i}] = Conta:{conta.Conta} - N° da Agência: {conta.Numero_agencia}");
+            }
+        }
     }
 }
